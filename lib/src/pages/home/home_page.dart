e93ee41837ecf8c0b63dce55a85a2da239e0afb3
LordGift <jarupath.j@cdg.co.th>
2021-04-20 16:28:00 +0700

    add ShopItems to Home

--- lib/src/pages/home/home_page.dart
+++ lib/src/pages/home/home_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_app/src/config/app_route.dart';
+import 'package:flutter_app/src/pages/login/background_theme.dart';
 import 'package:flutter_app/src/view_models/menu_viewmodel.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -8,46 +10,27 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       drawer: CommonDrawer(),
       appBar: AppBar(
-        title: Text('Home'),
+        title: Text('Home Page'),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+      body: GridView.builder(
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
         ),
+        itemBuilder: (context, index) => ShopListItem(200),
+        itemCount: 100,
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
+        onPressed: () {
+          //todo
+        },
         tooltip: 'Increment',
         child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      ),
     );
   }
 }
@@ -68,16 +51,21 @@ class CommonDrawer extends StatelessWidget {
             currentAccountPicture: CircleAvatar(
               backgroundImage: AssetImage('assets/images/flutter.png'),
             ),
+            decoration: BoxDecoration(gradient: BackGroundTheme.gradient),
           ),
-          ...MenuViewModel().items.map((e) => ListTile(
-            onTap: (){
+          ...MenuViewModel()
+              .items
+              .map((e) => ListTile(
+            onTap: () {
               e.onTap(context);
             },
             leading: Icon(
               e.icon,
               color: e.iconColor,
             ),
-          )),
+            title: Text(e.title),
+          ))
+              .toList(),
           Spacer(),
           ListTile(
             onTap: () {
@@ -89,6 +77,118 @@ class CommonDrawer extends StatelessWidget {
           )
         ],
       ),
+    );
+  }
+}
+
+class ShopListItem extends StatelessWidget {
+  final Function press;
+  final double maxHeight;
+
+  const ShopListItem(this.maxHeight, {Key key, this.press}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: press,
+      child: Container(
+        color: Colors.white,
+        child: Column(
+          children: <Widget>[
+            _buildImage(),
+            Expanded(
+              child: _buildInfo(),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Padding _buildInfo() => Padding(
+    padding: EdgeInsets.all(6),
+    child: Column(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Text(
+          'ชื่อสินค้า',
+          overflow: TextOverflow.ellipsis,
+          maxLines: 2,
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            Text(
+              '\$ ราคาสินค้า',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Text(
+              'จำนวน',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                color: Colors.deepOrangeAccent,
+              ),
+            )
+          ],
+        ),
+      ],
+    ),
+  );
+
+  Stack _buildImage() {
+    final height = maxHeight - 75;
+    final productImage =
+        'https://shortrecap.co/wp-content/uploads/2020/05/Catcover_web.jpg';
+    return Stack(
+      children: [
+        productImage != null && productImage.isNotEmpty
+            ? Image.network(
+          productImage,
+          height: height,
+          width: double.infinity,
+          fit: BoxFit.cover,
+        )
+            : Image.asset(
+          'xxx',
+          height: height,
+          width: double.infinity,
+        ),
+        0 > 1
+            ? SizedBox()
+            : Positioned(
+          top: 1,
+          right: 1,
+          child: Card(
+            color: Colors.white70,
+            child: Padding(
+              padding: const EdgeInsets.symmetric(
+                horizontal: 8,
+                vertical: 4,
+              ),
+              child: Row(
+                children: <Widget>[
+                  Icon(
+                    FontAwesomeIcons.box,
+                    size: 15.0,
+                    color: Colors.black,
+                  ),
+                  SizedBox(width: 4),
+                  Text(
+                    'out of stock',
+                    style: TextStyle(
+                      fontSize: 12,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
