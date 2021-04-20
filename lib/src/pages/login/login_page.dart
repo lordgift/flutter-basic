5da9ee23308b68a2eadec1b9b146b8af539d8028
LordGift <jarupath.j@cdg.co.th>
2021-04-20 13:45:10 +0700

    boxDecoration

--- lib/src/pages/login/login_page.dart
+++ lib/src/pages/login/login_page.dart
@@ -51,10 +51,11 @@ class LoginPage extends StatelessWidget {
                       ),
                     ),
                   ),
-                  SizedBox(
+                  Container(
+                    decoration: _boxDecoration(),
                     width: 280,
                     height: 52,
-                    child: ElevatedButton(
+                    child: TextButton(
                         onPressed: () {},
                         child: Text(
                           'Login',
@@ -71,6 +72,33 @@ class LoginPage extends StatelessWidget {
             ),
           )
         ],
+      ),
+    );
+  }
+  BoxDecoration _boxDecoration() {
+    final gradientStart = BackGroundTheme().gradientStart;
+    final gradientEnd = BackGroundTheme().gradientEnd;
+
+    final boxShadowItem = (Color color) => BoxShadow(
+      color: color,
+      offset: Offset(1.0, 6.0),
+      blurRadius: 20.0,
+    );
+
+    return BoxDecoration(
+      borderRadius: BorderRadius.all(Radius.circular(15.0)),
+      boxShadow: <BoxShadow>[
+        boxShadowItem(gradientStart),
+        boxShadowItem(gradientEnd),
+      ],
+      gradient: LinearGradient(
+        colors: [
+          gradientEnd,
+          gradientStart,
+        ],
+        begin: const FractionalOffset(0, 0),
+        end: const FractionalOffset(1.0, 1.0),
+        stops: [0.0, 1.0],
       ),
     );
   }
