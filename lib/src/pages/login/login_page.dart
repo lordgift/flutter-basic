e932c382b106e7c40e3e288216e201cb0168657a
LordGift <jarupath.j@cdg.co.th>
2021-04-21 13:48:51 +0700

    dispose

--- lib/src/pages/login/login_page.dart
+++ lib/src/pages/login/login_page.dart
@@ -18,6 +18,13 @@ class _LoginPageState extends State<LoginPage> {
   final _passwordController = TextEditingController();
 
   @override
+  void dispose() {
+    _usernameController?.dispose();
+    _passwordController?.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Stack(
